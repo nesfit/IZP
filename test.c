a1e80e510621b17cc08033b72754e0dc6218308a
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2022-11-27 23:34:51 +0100

    Create lab template with tests and instructions (#5)

--- test.c
+++ test.c
@@ -7,18 +7,144 @@
 
 #include "test.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+Person __load_person() {
+  int year;
+  char nameLoaded[100];
+  char *nameAllocated;
+  scanf("%d, %99s", &year, (char *)&nameLoaded);
+  nameAllocated = malloc(strlen(nameLoaded) + 1);
+  strcpy(nameAllocated, nameLoaded);
 
-#ifdef TEST_BUILD
+  Person p = {.birthYear=year, .name=nameAllocated};
+  return p;
+}
 
-int main(int argc, char *argv[]) {
-  print_args(argv, argc);
-  run_automated_tests();
+void __dispose_person(Person *p) {
+  if (p && p->name) {
+    free(p->name);
+    p->name = NULL;
+  }
+  p->birthYear = 0;
+}
+
+void __print_person(const char *prefix, Person *p) {
+  printf("%s", prefix);
+  person_print(p);
+  putchar('\n');
+}
+
+void __print_array(const char *prefix, PersonArray *array) {
+  printf("%s", prefix);
+  printf("PersonArray(%u): ", array->length);
+  array_print(array);
+}
+
+PersonArray __load_array() {
+  unsigned count;
+  scanf("load %u people:", &count);
+
+  PersonArray array = {.items=malloc(count * sizeof(Person)), .length=count};
+  for (size_t i = 0; i < array.length; i++) {
+    array.items[i] = __load_person();
+  }
+  //__print_array("loeaded: ", &array);
+  return array;
+}
+
+void __dispose_array(PersonArray *array) {
+  if (array && array->items != NULL) {
+    for (size_t i = 0; i < array->length; i++) {
+      __dispose_person(&array->items[i]);
+    }
+    free(array->items);
+    array->items = NULL;
+  }
+  array->length = 0;
+}
+
+int test_person_dtor()
+{
+  Person person = __load_person();
+  person_dtor(&person);
+  __print_person("result: ", &person);
+  __dispose_person(&person);
   return 0;
 }
 
-#endif
-
-void run_automated_tests() {
-  // TODO: add custom tests
+int test_array_dtor()
+{
+  PersonArray array = __load_array();
+  array_dtor(&array);
+  __print_array("result: ", &array);
+  __dispose_array(&array);
+  return 0;
 }
+
+int test_find_min()
+{
+  PersonArray array = __load_array();
+  int minItemIndex = array_find_min(&array, 0);
+  printf("found min item index: %d\n", minItemIndex);
+  if (minItemIndex >= 0) {
+    __print_person("result: ", &array.items[minItemIndex]);
+  }
+  __dispose_array(&array);
+  return 0;
+}
+
+int test_sort()
+{
+  PersonArray array = __load_array();
+  array_sort(&array);
+  __print_array("result: ", &array);
+  __dispose_array(&array);
+  return 0;
+}
+
+const char *test_names[] = {
+  "test_person_dtor",
+  "test_array_dtor",
+  "test_find_min",
+  "test_sort",
+};
+
+int (*tests[])() = {
+  &test_person_dtor,
+  &test_array_dtor,
+  &test_find_min,
+  &test_sort,
+};
+
+#define TEST_COUNT (sizeof(tests) / sizeof(*tests))
+
+int run_test_by_name(const char *test_name) {
+    if (test_name != NULL) {
+        for (size_t testId = 0; testId < TEST_COUNT; testId++)
+        {
+            if (strcmp(test_names[testId], test_name) == 0) {
+                return tests[testId]();
+            }
+        }
+    }
+    
+    fprintf(stderr, "could not find test '%s'\n", test_name);
+    fprintf(stderr, "supported tets:\n");
+    for (size_t testId = 0; testId < TEST_COUNT; testId++)
+    {
+        fprintf(stderr, "    - %s\n", test_names[testId]);
+    }
+
+    return 99;
+}
+
+#ifdef TEST_BUILD
+
+int main(int argc, char **args) {
+  const char *test_name = getenv("TEST_NAME");
+  return run_test_by_name(test_name);
+}
+
+#endif
--- types.h
+++ types.h
@@ -7,8 +7,54 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-// DEKLAROVANÉ HLAVIČKY FUNKCÍ NIJAK NEMĚŇTE
+// DEKLAROVANÉ HLAVIČKY FUNKCÍ A STRUKTUR NIJAK NEMĚŇTE
 
-void print_args(char **, int);
+typedef struct {
+  unsigned birthYear;
+  char *name;
+} Person;
+
+typedef struct {
+  unsigned length;
+  Person *items;
+} PersonArray;
+
+#pragma region Person methods
+
+void person_ctor(Person *p, unsigned birthYear, const char *name);
+
+void person_dtor(Person *p);
+
+void person_move(Person *src, Person *dst);
+
+void *person_copy(Person *src, Person *dst);
+
+void person_swap(Person *a, Person *b);
+
+int person_cmp(Person *a, Person *b);
+
+void person_print(Person *p);
+
+#pragma endregion
+
+#pragma region Person array methods
+
+void array_ctor(PersonArray *arrayrray);
+
+void array_dtor(PersonArray *arrayrray);
+
+void *array_expand(PersonArray *arrayrray);
+
+void *array_append(PersonArray *arrayrray, Person *p);
+
+void array_remove(PersonArray *arrayrray, unsigned int index);
+
+int array_find_min(PersonArray *arrayrray, int startIndex);
+
+void array_sort(PersonArray *arrayrray);
+
+void array_print(PersonArray *arrayrray);
+
+#pragma endregion
 
 #endif

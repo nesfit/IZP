e3c808dd835b8e96c63f88514121202222618ab3
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2023-09-05 18:15:58 +0200

    feat: add support for CLI arguments in test cases

--- test.c
+++ test.c
@@ -5,15 +5,19 @@
  *     OBSAH V TOMTO SOUBROU NEUPRAVUJTE!
  */
 
-#include "test.h"
+#include "types.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
-int test_example_success() {
+int test_example_success(int argc, char **argv) {
+  print_args(argv, argc);
   return 0;
 }
 
-int test_example_failure() {
+int test_example_failure(int argc, char **argv) {
+  print_args(argv, argc);
   return 1;
 }
 
@@ -22,19 +26,19 @@ const char *test_names[] = {
   "test_example_failure",
 };
 
-int (*tests[])() = {
+int (*tests[])(int, char**) = {
   &test_example_success,
   &test_example_failure,
 };
 
 #define TEST_COUNT (sizeof(tests) / sizeof(*tests))
 
-int run_test_by_name(const char *test_name) {
+int run_test_by_name(const char *test_name, int argc, char **argv) {
     if (test_name != NULL) {
         for (size_t testId = 0; testId < TEST_COUNT; testId++)
         {
             if (strcmp(test_names[testId], test_name) == 0) {
-                return tests[testId]();
+                return tests[testId](argc, argv);
             }
         }
     }
@@ -51,9 +55,9 @@ int run_test_by_name(const char *test_name) {
 
 #ifdef TEST_BUILD
 
-int main(int argc, char **args) {
+int main(int argc, char **argv) {
   const char *test_name = getenv("TEST_NAME");
-  return run_test_by_name(test_name);
+  return run_test_by_name(test_name, argc, argv);
 }
 
 #endif

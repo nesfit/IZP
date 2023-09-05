fd0cb9f832750c52bec65288276361a8f79d2f7c
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2023-09-05 16:33:08 +0200

    feat: improve testing capabilities, implement run-test-by-name

--- test.c
+++ test.c
@@ -9,16 +9,51 @@
 #include <stdio.h>
 
 
-#ifdef TEST_BUILD
-
-int main(int argc, char *argv[]) {
-  print_args(argv, argc);
-  run_automated_tests();
+int test_example_success() {
   return 0;
 }
 
-#endif
-
-void run_automated_tests() {
-  // TODO: add custom tests
+int test_example_failure() {
+  return 1;
 }
+
+const char *test_names[] = {
+  "test_example_success",
+  "test_example_failure",
+};
+
+int (*tests[])() = {
+  &test_example_success,
+  &test_example_failure,
+};
+
+#define TEST_COUNT (sizeof(tests) / sizeof(*tests))
+
+int run_test_by_name(const char *test_name) {
+    if (test_name != NULL) {
+        for (size_t testId = 0; testId < TEST_COUNT; testId++)
+        {
+            if (strcmp(test_names[testId], test_name) == 0) {
+                return tests[testId]();
+            }
+        }
+    }
+    
+    fprintf(stderr, "could not find test '%s'\n", test_name);
+    fprintf(stderr, "supported tets:\n");
+    for (size_t testId = 0; testId < TEST_COUNT; testId++)
+    {
+        fprintf(stderr, "    - %s\n", test_names[testId]);
+    }
+
+    return 99;
+}
+
+#ifdef TEST_BUILD
+
+int main(int argc, char **args) {
+  const char *test_name = getenv("TEST_NAME");
+  return run_test_by_name(test_name);
+}
+
+#endif

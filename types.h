c4737526e22e3e55121062d74466afdaa4613acf
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2023-09-06 13:00:35 +0200

    feat: implement lab assignment and tests

--- types.h
+++ types.h
@@ -7,8 +7,35 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+// maximální počet prvků v množině
+// (za použití všech dostupných prvků)
+#define MAXITEMS 10
+
+// zcela všechny prvky, které nám jsou ve všech množinách k dispozici
+extern const int UNIVERSUM[MAXITEMS];
+
+// TODO: 1. seznamte se s definovanými datovými typy
+
+typedef struct {
+  // prvky množiny, MAXITEMS určuje maximální počet,
+  //  ne ovšem ten skutečný
+  int items[MAXITEMS];
+  // skutečný počet prvků v množině, vždy platí:
+  //  0 <= cardinality <= MAXITEMS
+  int cardinality;
+} Set;
+
+typedef struct {
+  int first;  // první prvek dvojice
+  int second; // druhý prvek dvojice
+} Pair;
+
 // DEKLAROVANÉ HLAVIČKY FUNKCÍ NIJAK NEMĚŇTE
 
-void print_args(char **argv, int argc);
+int rel_isFunction(Pair pairs[], int pairCount, Set set);
+
+int rel_minMax(Pair pairs[], int pairCount, int *relMin, int *relMax);
+
+int rel_isEquivalence(Pair pairs[], int pairCount, Set set);
 
 #endif

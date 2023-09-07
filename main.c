b9cbbd4770771a77a4cd92f46a2a1bec465a9211
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2023-09-07 14:23:22 +0200

    feat: implement lab assignment and tests

--- main.c
+++ main.c
@@ -2,19 +2,62 @@
 #include <stdio.h>
 
 
-void print_args(char **argv, int argc) {
-  printf("argc=%d\n", argc);
-  for (int argi = 1; argi < argc; argi++) {
-    printf("argv[%d]=%s\n", argi, argv[argi]);
-  }
+/**
+ * Determine whether the provided character is alphabetic.
+ * 
+ * @param c character
+ * 
+ * @return true when the provided character `c` is alphabetic;
+ *    false otherwise
+ */
+bool is_alpha(char c) {
+  // TODO: 1. Check whether the value in c represents an alphabetic ASCII character
+  return false;
+}
+
+/**
+ * Determine whether all characters of the provided `string`
+ *    (character array) are alphabetic.
+ * 
+ * @param string input string
+ * 
+ * @return true when all characters in the provided string are alphabetic;
+ * 	  false otherwise
+ */
+bool is_name(char string[]) {
+	// TODO: 1. For each character in string
+	//   - determine whether it is alphabetic by calling is_aplha function
+	return false;
 }
 
 #ifndef TEST_BUILD
 
-int main(int argc, char *argv[]) {
-  print_args(argv, argc);
-  run_automated_tests();
-  return 0;
+int main() {
+	// Limit the size of names to 20 characters + terminating '\0'
+	char name[21];
+	char surname[21];
+
+	// Let the user specify their name
+	printf("Please provide your name: ");
+	scanf("%20s", name);
+
+	// Call our new function that checks if the name contains only alphabetic symbols
+	// The argument supplied to the function can have a different name than the one
+	// specified in the function definition.
+	if(!is_name(name)) {
+		printf("Provided name contains non-alphabetic characters!\n");
+		return 1;
+	}
+
+	printf("Please provide your surname: ");
+	scanf("%20s", surname);
+	if(!is_name(surname)) {
+		printf("Provided surname contains non-alphabetic characters!\n");
+		return 1;
+	}
+
+	printf("Your full name is: %s %s\n", name, surname);
+	return 0;
 }
 
 #endif

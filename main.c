9a6e2c0c0adb87e2b36f49a9dddc5e238dbf3524
Jan Zavrel <zavrel@netsearch.cz>
2023-09-08 15:34:56 +0200

    feat: Implemented lab assignment

--- main.c
+++ main.c
@@ -1,19 +1,62 @@
 #include "types.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 
-void print_args(char **argv, int argc) {
-  printf("argc=%d\n", argc);
-  for (int argi = 1; argi < argc; argi++) {
-    printf("argv[%d]=%s\n", argi, argv[argi]);
-  }
+/**
+ * Safely print first argument provided to the program. Determine its meaning.
+ * 
+ * @param argc total number of arguments provided to the program
+ * 
+ * @param argv array/vector of arguments
+*/
+void print_first_argument(int argc, char *argv[]) {
+
+  return;
 }
+
+/**
+ * Safely print first 4 arguments provided to the program while converting them to their corresponding data type.
+ * 
+ * ./main <int> <double> <string>
+ * 
+ * To convert <string> to <int>, use function atoi() (https://cplusplus.com/reference/cstdlib/atoi/)
+ * 
+ * To convert <string> to <double>, use function atof() (https://cplusplus.com/reference/cstdlib/atof/)
+ * 
+ * Use the following commmand to execute your program with set of arguments: 
+ * 
+ *      ./main 42 13.37 hello_program 
+ * 
+ * @param argc total number of arguments provided to the program
+ * 
+ * @param argv array/vector of arguments
+*/
+void print_parse_arguments(int argc, char *argv[]) {
+
+	return;
+}
+
+/**
+ * Safely print ALL arguments provided to the program without any conversion.
+ * 
+ * ./main <int> <double> <string>
+ * 
+ * @param argc total number of arguments provided to the program
+ * 
+ * @param argv array/vector of arguments
+*/
+void print_all_arguments(int argc, char *argv[]) {
+  
+  return;
+}
+
 
 #ifndef TEST_BUILD
 
 int main(int argc, char *argv[]) {
-  print_args(argv, argc);
-  run_automated_tests();
+
+  //TODO: 1. Call functions and pass argc and argv
   return 0;
 }
 

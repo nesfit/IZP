df948254b8da6ac2b5f72a0b30910828057e83df
Jan Zavrel <zavrel@netsearch.cz>
2023-09-08 20:01:09 +0200

    feat: Implement lab assignment

--- types.h
+++ types.h
@@ -8,7 +8,11 @@
 #define TYPES_H
 
 // DEKLAROVANÉ HLAVIČKY FUNKCÍ NIJAK NEMĚŇTE
-
-void print_args(char **argv, int argc);
+void print_helloworld();
+void print_discriminant();
+void read_print_is_even();
+void read_print_is_in_interval();
+void read_print_is_leap_year();
+void read_print_maximum();
 
 #endif

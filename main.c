1fabdcac378266a502dfc6064ccd6a60bfb4af2d
Jan Zavrel <zavrel@netsearch.cz>
2023-09-08 12:18:25 +0200

    feat: Implement lab assignment

--- main.c
+++ main.c
@@ -1,19 +1,54 @@
 #include "types.h"
 #include <stdio.h>
 
-
-void print_args(char **argv, int argc) {
-  printf("argc=%d\n", argc);
-  for (int argi = 1; argi < argc; argi++) {
-    printf("argv[%d]=%s\n", argi, argv[argi]);
-  }
+void read_print_string() {
+  //TODO: 1. Read a string from stdin of max-length of 100 characters.
+  
+  //TODO: 2. Print read string to stdout.
+  return;
 }
+
+void read_print_string_length() {
+  //TODO: 1. Read a string from stdin of max-length of 100 characters.
+  
+  //TODO: 2. Print the length of the read string to stdout. (do not count '\0').
+  return;
+}
+
+void read_print_string_alphnum() {
+  //TODO: 1. Read a string from stdin of max-length of 100 characters.
+  
+  //TODO: 2. Print both the number of letters (a-z, A-Z) and the number of numerals (0-9) in the read string to stdout.
+  return;
+}
+
+void read_print_string_lower() {
+  //TODO: 1. Read a string from stdin of max-length of 100 characters.
+  
+  //TODO: 2. Print the read string with all letters in lowercase (e.g., A => a) to stdout.
+  return;
+}
+
+void read_print_string_replace() {
+  //TODO: 1. Read a character and a string of max-length of 100 characters from stdin.
+  
+  //TODO: 2. Print the read string with all occurences of read string replaced with '_' (e.g., ('a', "abba") => _bb_) to stdout.
+  return;
+}
+
+
+void read_print_string_compare() {
+  //TODO: 1. Read  2 string, each of max-length of 100 characters, from stdin.
+  
+  //TODO: 2. Determine whether strings are equal (two strings are equal if they have the same length and characters in both strings on a given index are identical).
+  return;
+}
+
 
 #ifndef TEST_BUILD
 
 int main(int argc, char *argv[]) {
-  print_args(argv, argc);
-  run_automated_tests();
+
   return 0;
 }
 

5a65e6ba2f19f1b5dd40662cc27dfdd225d4fad5
Daniel Dolejška <dolejska@fit.vut.cz>
2024-09-17 09:57:34 +0200

    feat: update script config for JupyterHub deploy

--- .tests/config.sh
+++ .tests/config.sh
@@ -1,10 +1,27 @@
 
 PROJECT_SUBMIT_SOURCES=("main.c")
 
+# REPLIT.COM configuration
 PROJECT_GROUP_ID="$REPL_OWNER"
 PROJECT_TASK_ID="$REPL_SLUG"
 PROJECT_AUTHOR="$LOGIN"
 
+if [[ -z "$PROJECT_TASK_ID" && -n "$IZP_BRANCH" ]]; then
+    # try getting task ID from JupyterHub environment
+    PROJECT_TASK_ID="${IZP_BRANCH:0:5}"
+
+    if [[ -z "$PROJECT_GROUP_ID" ]]; then
+        PROJECT_GROUP_ID="#detect"
+    fi
+fi
+
+if [[ -z "$LOGIN" && "$JUPYTERHUB_USER" ==  *"@vutbr.cz" ]] \
+|| [[ -z "$LOGIN" && "$JUPYTERHUB_USER" ==  *"@vut.cz" ]]; then
+    # try getting login from JupyterHub environment
+    LOGIN="${JUPYTERHUB_USER%%@*}"
+fi
+
+# variable defaults
 [ -z "$PROJECT_SUBMIT_MODE" ] && [ -n "$PROJECT_TASK_ID" ] && PROJECT_SUBMIT_MODE="tests"
 [ -z "$PROJECT_GROUP_ID" ] && PROJECT_GROUP_ID="github.com-nesad-IZP"
 [ -z "$PROJECT_TASK_ID" ] && PROJECT_TASK_ID="local-dev"

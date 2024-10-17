7986a2ac285879a9c70512d2d9c26468b5f32902
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2024-10-17 14:02:10 +0200

    style: format

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
 #include "types.h"
 #include <stdio.h>
 
-
 /**
  * Print the contents of the provided 2D matrix.
- * 
+ *
  * @param arr 2D matrix
  */
 void print_2d(int arr[MAT_ROWS][MAT_COLUMNS]) {
@@ -16,7 +15,7 @@ void print_2d(int arr[MAT_ROWS][MAT_COLUMNS]) {
 
 /**
  * Search for a value in the provided 2D matrix.
- * 
+ *
  * @param arr 2D matrix
  * @returns true when found, false otherwise
  */
@@ -27,64 +26,64 @@ bool contains_value(int arr[MAT_ROWS][MAT_COLUMNS], int value) {
 
 /**
  * Search for a value in the provided 2D matrix.
- * 
+ *
  * @param arr 2D matrix
  * @param value the value to find in the matrix
- * 
+ *
  * @return coordinates of the value in the matrix when found;
  *     corrdinates {-1, -1} otherwise
  */
 MatCoords find_value(int arr[MAT_ROWS][MAT_COLUMNS], int value) {
   // TODO: 3. try to locate the provided value in the provided array
   //    and return its coordinates when found
-  return (MatCoords) {-2, -2};
+  return (MatCoords){-2, -2};
 }
 
 #ifndef TEST_BUILD
 
 int main(void) {
-    // Create 3x3 array (matrix, table, ...)
-    // The array has 3 rows and in each row, 3 columns
-    // MAT_ROWS and MAT_COLUMNS are defined in ./types.h
-    int matrix[MAT_ROWS][MAT_COLUMNS], value;
+  // Create 3x3 array (matrix, table, ...)
+  // The array has 3 rows and in each row, 3 columns
+  // MAT_ROWS and MAT_COLUMNS are defined in ./types.h
+  int matrix[MAT_ROWS][MAT_COLUMNS], value;
 
-    // TODO: Initialize the 2d array by setting each element to 0, i.e.:
-    // --- C_0 C_1 C_2
-    // R_0  0   0   0 
-    // R_1  0   0   0
-    // R_2  0   0   0
+  // TODO: Initialize the 2d array by setting each element to 0, i.e.:
+  // --- C_0 C_1 C_2
+  // R_0  0   0   0
+  // R_1  0   0   0
+  // R_2  0   0   0
 
-    print_2d(matrix);
+  print_2d(matrix);
 
-    int identity_matrix[MAT_ROWS][MAT_COLUMNS];
-    // TODO: Create and initialize an identity matrix, i.e.:
-    // --- C_0 C_1 C_2
-    // R_0  1   0   0 
-    // R_1  0   1   0
-    // R_2  0   0   1
+  int identity_matrix[MAT_ROWS][MAT_COLUMNS];
+  // TODO: Create and initialize an identity matrix, i.e.:
+  // --- C_0 C_1 C_2
+  // R_0  1   0   0
+  // R_1  0   1   0
+  // R_2  0   0   1
 
-    print_2d(identity_matrix);
+  print_2d(identity_matrix);
 
-    // Set new value to row = 1, col = 2
-    value = matrix[1][2] = 5;
+  // Set new value to row = 1, col = 2
+  value = matrix[1][2] = 5;
 
-    // Try to find the value
-    bool contains_result = contains_value(matrix, value);
-    if (contains_result) {
-        printf("Value: %d found!\n", value);
-    } else {
-        printf("Value: %d not found!\n", value);
-    }
+  // Try to find the value
+  bool contains_result = contains_value(matrix, value);
+  if (contains_result) {
+    printf("Value: %d found!\n", value);
+  } else {
+    printf("Value: %d not found!\n", value);
+  }
 
-    // Try to find the value and obtain its coordinates in the matrix
-    MatCoords coords = find_value(matrix, value);
-    if (coords.row == -1 || coords.column == -1) {
-        printf("Value: %d not found!\n", value);
-    } else {
-        printf("Value: %d found at [%d, %d].\n", value, coords.row, coords.column);
-    }
+  // Try to find the value and obtain its coordinates in the matrix
+  MatCoords coords = find_value(matrix, value);
+  if (coords.row == -1 || coords.column == -1) {
+    printf("Value: %d not found!\n", value);
+  } else {
+    printf("Value: %d found at [%d, %d].\n", value, coords.row, coords.column);
+  }
 
-    return 0;
+  return 0;
 }
 
 #endif
--- types.h
+++ types.h
@@ -17,26 +17,25 @@
  * Structure representing coordinates in a generic 2D matrix;
  */
 struct mat_coords_t {
-    /** Row index in the matrix */
-    int row;
-    /** Column index in the matrix row */
-    int column;
+  /** Row index in the matrix */
+  int row;
+  /** Column index in the matrix row */
+  int column;
 };
 
 // create `MatCoords` type as an alias for `struct mat_coords_t`
 typedef struct mat_coords_t MatCoords;
 
-
 /**
  * Print the contents of the provided 2D matrix.
- * 
+ *
  * @param matrix 2D matrix
  */
 void print_2d(int matrix[MAT_ROWS][MAT_COLUMNS]);
 
 /**
  * Search for a value in the provided 2D matrix.
- * 
+ *
  * @param matrix 2D matrix
  * @returns 1 when found, 0 otherwise
  */
@@ -44,10 +43,10 @@ bool contains_value(int matrix[MAT_ROWS][MAT_COLUMNS], int value);
 
 /**
  * Search for a value in the provided 2D matrix.
- * 
+ *
  * @param matrix 2D matrix
  * @param value the value to find in the matrix
- * 
+ *
  * @return coordinates of the value in the matrix when found;
  *     corrdinates {-1, -1} otherwise
  */

140919dfca3ab5a4c9532833f9d9a76581fae734
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2025-09-17 08:27:18 +0200

    fix: properly declare function prototypes

--- main.c
+++ main.c
@@ -1,35 +1,35 @@
 #include "types.h"
 #include <stdio.h>
 
-void read_print_string() {
+void read_print_string(void) {
   //TODO: 1. Read a string from stdin of max-length of 100 characters.
   
   //TODO: 2. Print read string to stdout.
   return;
 }
 
-void read_print_string_length() {
+void read_print_string_length(void) {
   //TODO: 1. Read a string from stdin of max-length of 100 characters.
   
   //TODO: 2. Print the length of the read string to stdout. (do not count '\0').
   return;
 }
 
-void read_print_string_alphnum() {
+void read_print_string_alphnum(void) {
   //TODO: 1. Read a string from stdin of max-length of 100 characters.
   
   //TODO: 2. Print both the number of letters (a-z, A-Z) and the number of numerals (0-9) in the read string to stdout.
   return;
 }
 
-void read_print_string_lower() {
+void read_print_string_lower(void) {
   //TODO: 1. Read a string from stdin of max-length of 100 characters.
   
   //TODO: 2. Print the read string with all letters in lowercase (e.g., A => a) to stdout.
   return;
 }
 
-void read_print_string_replace() {
+void read_print_string_replace(void) {
   //TODO: 1. Read a character and a string of max-length of 100 characters from stdin.
   
   //TODO: 2. Print the read string with all occurences of read string replaced with '_' (e.g., ('a', "abba") => _bb_) to stdout.
@@ -37,7 +37,7 @@ void read_print_string_replace() {
 }
 
 
-void read_print_string_compare() {
+void read_print_string_compare(void) {
   //TODO: 1. Read  2 string, each of max-length of 100 characters, from stdin.
   
   //TODO: 2. Determine whether strings are equal (two strings are equal if they have the same length and characters in both strings on a given index are identical).
--- types.h
+++ types.h
@@ -8,11 +8,11 @@
 #define TYPES_H
 
 // DEKLAROVANÉ HLAVIČKY FUNKCÍ NIJAK NEMĚŇTE
-void read_print_string();
-void read_print_string_length();
-void read_print_string_alphnum();
-void read_print_string_lower();
-void read_print_string_replace();
-void read_print_string_compare();
+void read_print_string(void);
+void read_print_string_length(void);
+void read_print_string_alphnum(void);
+void read_print_string_lower(void);
+void read_print_string_replace(void);
+void read_print_string_compare(void);
 
 #endif
